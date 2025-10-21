370d2c79965acb7e7211662bf5dfb27af4edf7a2
KriptoBaba382 <199w29c@mail.ru>
2025-10-21 11:35:03 +0300

    add element.style.cssText

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ function DomElement(selector, height, width, bg, fontSize) {
     this.bg = bg;
     this.fontSize = fontSize;
 }
-DomElement.prototype.createElement = function() {
+
+DomElement.prototype.createElement = function () {
     let element;
-    
+
     if (this.selector.startsWith('.')) {
         element = document.createElement('div');
         element.className = this.selector.slice(1);
@@ -17,5 +18,24 @@ DomElement.prototype.createElement = function() {
     } else {
         console.error('Селектор должен начинаться с . или #');
         return null;
-    },
-    
+    }
+
+    element.style.cssText = `
+        height: ${this.height}px;
+        width: ${this.width}px;
+        background: ${this.bg};
+        font-size: ${this.fontSize}px;
+        margin: 10px 0;
+        padding: 20px;
+        border-radius: 5px;
+        box-sizing: border-box;
+    `;
+
+    element.textContent = `Это элемент с селектором: ${this.selector}`;
+    document.body.appendChild(element);
+
+    return element;
+};
+
+const myElement = new DomElement('.my-block', 120, 200, 'lightcoral', 16);
+myElement.createElement();
