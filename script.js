c650d6b795e4057ba8548fd0eb9ec1fc6af101bf
KriptoBaba382 <199w29c@mail.ru>
2025-10-27 18:00:07 +0300

    add: class constructor

--- script.js
+++ script.js
@@ -1,41 +1,26 @@
-function DomElement(selector, height, width, bg, fontSize) {
-    this.selector = selector;
-    this.height = height;
-    this.width = width;
-    this.bg = bg;
-    this.fontSize = fontSize;
+'use strict'
+
+class Person {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+        Person.incrementCount()
+    }
+    static count = 0
+
+    static getCount() {
+        return Person.count
+    }
+    static incrementCount() {
+        Person.count++
+    }
+    sayHello() {
+        console.log(`Привет! Меня зовут ${this.name}`);
+    }
 }
 
-DomElement.prototype.createElement = function () {
-    let element;
-
-    if (this.selector.startsWith('.')) {
-        element = document.createElement('div');
-        element.className = this.selector.slice(1);
-    } else if (this.selector.startsWith('#')) {
-        element = document.createElement('p');
-        element.id = this.selector.slice(1);
-    } else {
-        console.error('Селектор должен начинаться с . или #');
-        return null;
-    }
-
-    element.style.cssText = `
-        height: ${this.height}px;
-        width: ${this.width}px;
-        background: ${this.bg};
-        font-size: ${this.fontSize}px;
-        margin: 10px 0;
-        padding: 20px;
-        border-radius: 5px;
-        box-sizing: border-box;
-    `;
-
-    element.textContent = `Это элемент с селектором: ${this.selector}`;
-    document.body.appendChild(element);
-
-    return element;
-};
-
-const myElement = new DomElement('.my-block', 120, 200, 'lightcoral', 16);
-myElement.createElement();
+const person = new Person('Vlad', 23)
+// console.log(JSON.stringify(person).length)
+console.log(person)
+person.sayHello()
+console.log(person);
