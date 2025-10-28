7aa36cb012cf2454bb64465f18e7792b27d4be19
KriptoBaba382 <199w29c@mail.ru>
2025-10-28 11:16:36 +0300

    add class First and class Second

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
-'use strict'
+// 'use strict'
 
-class Person {
-    constructor(name, age) {
-        this.name = name
-        this.age = age
-        Person.incrementCount()
-    }
-    static count = 0
+// class Person {
+//     constructor(name, age) {
+//         this.name = name
+//         this.age = age
+//         Person.incrementCount()
+//     }
+//     sayHello() {
+//         console.log(`Привет! Меня зовут ${this.name}`);
+//     }
+// }
 
-    static getCount() {
-        return Person.count
-    }
-    static incrementCount() {
-        Person.count++
-    }
-    sayHello() {
-        console.log(`Привет! Меня зовут ${this.name}`);
+// const person = new Person('Vlad', 23)
+// console.log(person)
+// person.sayHello()
+
+// class FrontEndDev extends Person {
+//     constructor(name, age, skills = []) {
+//         super()
+//         this._skills = skills
+//     }
+//     get skills() {
+//         return this._skills
+//     }
+// }
+
+// const dev = new FrontEndDev('Vlad', 23)
+
+// console.log(dev);
+// dev.sayHello()
+
+class First {
+    hello() {
+        console.log("Привет я метод родителя!");
     }
 }
 
-const person = new Person('Vlad', 23)
-// console.log(JSON.stringify(person).length)
-console.log(person)
-person.sayHello()
-console.log(person);
+class Second extends First {
+    hello() {
+        super.hello();
+        console.log("А я наследуемый метод!");
+    }
+}
